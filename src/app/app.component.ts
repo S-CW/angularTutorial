2c3cf2a1bf13f8a05106ccd013ac1a4915640310
Jeffrey Sing <jeffreysing1188@gmail.com>
2024-06-18 17:47:39 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from './models/wishItem';
+import { FormsModule } from '@angular/forms';
+import { WishListComponent } from './component/wish-list/wish-list.component';
+import { AddWishFormComponent } from './component/add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './component/wish-filter/wish-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'AngularTutorial2';
+  items: WishItem[] = [
+    new WishItem('Laundry'),
+    new WishItem('Play games', true),
+    new WishItem('Learn Angular', true),
+  ];
+
+  filter: any = () => {};
 }
