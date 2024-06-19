46fb673dacd1c4029b99cb2162d54cb614d006ff
Jeffrey Sing <jeffreysing1188@gmail.com>
2024-06-19 14:20:41 +0800

    styling for completed wish list

--- src/app/component/wish-list-item/wish-list-item.component.ts
+++ src/app/component/wish-list-item/wish-list-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { WishItem } from '../../models/wishItem';
 import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'wish-list-item',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css',
 })
@@ -14,6 +15,13 @@ export class WishListItemComponent {
 
   @Input() fulfilled!: boolean;
   @Output() fulfilledChange = new EventEmitter<boolean>();
+
+  get cssClasses() {
+    /*ternary
+    return this.fulfilled ? ['strikeout', 'text-muted'] : []; */
+
+    return { 'strikeout text-muted': this.fulfilled };
+  }
 
   toggleFulfilled() {
     this.fulfilled = !this.fulfilled;
